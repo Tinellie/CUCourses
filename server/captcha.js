8194bfb6c85ae259f1d3092e52c0fe2ac814f959
Tinellie <akizukimiu23@gmail.com>
2024-10-15 15:13:16 +0800

    [feat] add CaptchaHelper

--- server/captcha.js
+++ server/captcha.js
@@ -3,53 +3,81 @@ const colors = require("colors-console");
 const {log} = require("debug");
 const Jimp = require("jimp");
 
-async function RecognizeCaptcha(buffer) {
-    const worker = await createWorker("eng", 1, {
-        // logger: m => console.log(m),
-    });
-    // await worker.setParameters({
-    //     tessedit_char_whitelist: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    // })
-    const {data: {text}} = await worker.recognize(buffer);
-    console.log(colors("yellow", "CAPTCHA RECOGNIZED: " + text));
-    await worker.terminate();
-    return text;
-}
+
+class CaptchaHelper {
+    constructor() {
+        this.black = [0, 0, 0];
+        this.white = [255, 255, 255];
+    }
 
 
-async function RecognizeRotated(img, step, range) {
-    const worker = await createWorker("eng");
+    async Binarization(img) {
+        let scan = (f) => img.scan(0, 0, img.bitmap.width, img.bitmap.height, (x, y, idx) => {
+            const r = img.bitmap.data[idx];
+            const g = img.bitmap.data[idx + 1];
+            const b = img.bitmap.data[idx + 2];
+            [
+                img.bitmap.data[idx],
+                img.bitmap.data[idx + 1],
+                img.bitmap.data[idx + 2]
+            ] = f(r, g, b);
+        });
 
+        img.blur(1);
+        scan((r, g, b) => {
+            const t = 108;
+            return (r < t && g < t && b < t) ? this.black : this.white
+        });
+        return img;
+    }
 
-    let currentCount = 0, current = '';
-    let maxCount = 0, max = '';
-
-    for (let i = -range/2; i <= range/2; i += step) {
-        let {data: {text}}
-            = await worker.recognize(
-                await img.clone().rotate({deg:i}).getBuffer("image/png")
-            );
-        // console.log(`len: ${text.length}, match: ${text.match(/[a-z0-9]/i)}`);
-        text = text.replaceAll(/[\s\n\r]/g, "");     // remove whitespace
-        // console.log(colors("yellow", "trial: " + text));
-
-        // console.log(`len: ${text.length}, match: ${text.match(/[a-z0-9]/i)}`);
-        // console.log(text[1].charCodeAt(0));
-        // console.log(text.match(/[\s\n\r]/g));
-        // console.log(`current: [${currentCount}, ${current}] max: [${maxCount}, ${max}], \"${text}\"`);
-        if (text.length === 1 && text.match(/[a-z0-9]/i)) {
-            if (text !== current)
-                [currentCount, current] = [0, text];
-            currentCount++;
-            if (currentCount > maxCount)
-                [max, maxCount] = [current, currentCount];
+    async CropRecognize(img, n, f) {
+        let l = []
+        for (let i = 0; i < n; i++) {
+            l.push(await f(await img.clone().crop({x: img.width/n * i+2, y: 0, w: img.width/4-4, h: img.height})));
         }
     }
-    console.log(colors("yellow", "CAPTCHA RECOGNIZED: " + max));
+
+    async RecognizeCaptcha(buffer) {
+        const worker = await createWorker("eng");
+
+        const {data: {text}} = await worker.recognize(buffer);
+        console.log(colors("yellow", "CAPTCHA RECOGNIZED: " + text));
+        await worker.terminate();
+        return text;
+    }
 
 
-    await worker.terminate();
-    return max;
+    async RecognizeRotated(img, step, range) {
+        const worker = await createWorker("eng");
+
+
+        let currentCount = 0, current = '';
+        let maxCount = 0, max = '';
+
+        for (let i = -range / 2; i <= range / 2; i += step) {
+            let {data: {text}}
+                = await worker.recognize(
+                await img.clone().rotate({deg: i}).getBuffer("image/png")
+            );
+
+            text = text.replaceAll(/[\s\n\r]/g, "");     // remove whitespace
+            // console.log(colors("yellow", "trial: " + text));
+
+            if (text.length === 1 && text.match(/[a-z0-9]/i)) {
+                if (text !== current)
+                    [currentCount, current] = [0, text];
+                currentCount++;
+                if (currentCount > maxCount)
+                    [max, maxCount] = [current, currentCount];
+            }
+        }
+        if(max.match(/[cov]/)) max.toUpperCase();
+        console.log(colors("yellow", "CAPTCHA RECOGNIZED: " + max));
+
+
+        await worker.terminate();
+        return max;
+    }
 }
-
-module.exports = { RecognizeCaptcha, RecognizeRotated };
+module.exports = new CaptchaHelper();
--- server/routes/screenshot.js
+++ server/routes/screenshot.js
@@ -42,71 +42,14 @@ router.get('/', async (req, res) => {
     let img = await Jimp.read("image/captcha.png")
     // img.greyscale();
     // img.contrast(-0.5);
-    const [black, white] = [[0, 0, 0], [255, 255, 255]]
 
-
-    let scan = (f) => img.scan(0, 0, img.bitmap.width, img.bitmap.height, (x, y, idx) => {
-        const r = img.bitmap.data[idx];
-        const g = img.bitmap.data[idx + 1];
-        const b = img.bitmap.data[idx + 2];
-        // const avg = (red + green + blue) / 3;
-        // const threshold = 158; // 阈值
-        // console.log(colors("red", `${idx}`));
-        [
-            img.bitmap.data[idx],
-            img.bitmap.data[idx + 1],
-            img.bitmap.data[idx + 2]
-        ] = f(r, g, b);
-    });
-
-    // scan((r, g, b) => {
-    //     const t = 5;
-    //     return (Math.abs(r-g) > t || Math.abs(g-b) > t || Math.abs(b-r) > t) ?
-    //         black : white;
-    // });
-    img.blur(1);
-    scan((r, g, b) => {
-        const t = 105;
-        return (r < t && g < t && b < t) ? black : white
-    })
-    // img.blur(1);
-    // scan((r, g, b) => {
-    //     const t = 105;
-    //     return (r < t && g < t && b < t) ? black : white
-    // })
-    // let m = 1, f=4;
-    // let img2
-    //     = new Jimp({width: img.width + 2*m, height: img.height + 2*m, color: '#ffffff'});
-    // await img2.blit({src: img, x: m, y: m})
-    //
-    // await img2.resize({w: img2.width * f, h: img2.height * f});
-    //
-    // await img2.crop({x:img2.width *2 / 4, y:0, w: img2.width / 4 + 30, h: img2.height});
-    // await img2.write("image/empty.png");
-
-
-    // img.crop({x: -m, y: -m, w: img.width + 2*m, h: img.height + 2*m });
-    //
-    // async function recognize(f, i){
-    //     let img2 = await f(img.clone());
-    //     await img2.write(`image/captcha/${i}.png`);
-    //     return await cap.RecognizeCaptcha(await img2.getBuffer("image/png"));
-    // }
-    //
-    // let range = 60;
-    // let d = 5;
-    // for (let i = -range/2; i <= range/2; i+=d) {
-    //     await recognize(async img => await img.rotate({deg:i}), i);
-    // }
+    img = await cap.Binarization(img);
 
     await img.write("image/captcha2.png");
     let img_buffer = fs.readFileSync("image/captcha2.png");
 
-    for (let i = 0; i < 4; i++) {
-        await cap.RecognizeRotated(
-            await img.clone().crop({x: img.width/4 * i+2, y: 0, w: img.width/4-4, h: img.height}), 5, 60
-        );
-    }
+    let captcha =
+        cap.CropRecognize(img, 4, (img) => cap.RecognizeRotated(img, 5, 60));
 
 
     screenshotBuffer = img_buffer;
